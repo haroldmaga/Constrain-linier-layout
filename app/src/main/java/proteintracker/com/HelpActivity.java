b7f6b7450719f02f1325bad4086d3f092059bfc4
Eldrin <@Eldrin.com>
2019-09-04 17:46:39 +0700

    Initial commit

---
+++ app/src/main/java/proteintracker/com/HelpActivity.java
@@ -0,0 +1,27 @@
+package proteintracker.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+
+        super.onCreate(savedInstanceState);
+
+        TextView myTextView = new TextView(this);
+        myTextView.setText("Some Help Text");
+        setContentView(myTextView);
+
+
+        }
+}
